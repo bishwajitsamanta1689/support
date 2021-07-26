caed808dc1988b7465489da2be6e2a90951dd26f
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2021-07-26 21:11:12 +0530

    CIFS Check Modified

--- RABIA/cifscheck.sh
+++ RABIA/cifscheck.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "`date` Cifs Check Started.."
-
 fstabCheck=$(cat /etc/fstab | grep -v '#' | grep -i cifs | wc -l)
 diskCheck=$(df -hT | grep -i cifs | wc -l)
 
 if [ $fstabCheck == $diskCheck ]
 then
-  echo "`date` All CIFS Mounts are correctly Mounted."
+  echo "`date` Cifs Check Started.."
+  echo "`date` All CIFS Mounts are correctly Mounted." >> /tmp/cifs_mount.txt
 else
-  echo "`date` Need to check CIFS Mounts!"
+  echo "`date` Cifs Check Started.."
+  echo "`date` Need to check CIFS Mounts!" >> /tmp/cifs_not_mount.txt
 fi
 
